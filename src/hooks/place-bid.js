1ef2ef08f7e9a0294b8424d9b697b120c214bbbc
Muhammad Ahmed <ahmede7th@gmail.com>
2018-04-11 18:47:13 -0400

    fixed bug which allowed you to enter negative numbers for bids

--- src/hooks/place-bid.js
+++ src/hooks/place-bid.js
@@ -1,5 +1,18 @@
+const FeathersError  = require('@feathersjs/errors');
+
+
 module.exports = function (options = {}) {
   return async context => {
+
+    if (parseFloat (context.data.current_price) <= 0)
+    {
+      throw new FeathersError.BadRequest('Invalid Parameters', {
+        errors: { current_price: 'price has to be greater than 0' }
+      });
+    }
+
+
+
     return context.app.service('auctions')
     .get(context.id)
     .then(auction => {
